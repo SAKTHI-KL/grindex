21a8eada73c0161b8a129e39fbd8c4bab0b8e970
SAKTHI-KL <81792430+SAKTHI-KL@users.noreply.github.com>
2024-03-23 13:14:26 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -64,7 +64,7 @@ function showSlide(n) {
 let popslideIndex = [1,1];
 let slideId = ["mySlides1", "mySlides11"]
 showpopSlides(1, 0);
-showpopSlides(1, 1);
+// showpopSlides(1, 1);
 
 function pluspopSlides(n, no) {
   showpopSlides(popslideIndex[no] += n, no);
